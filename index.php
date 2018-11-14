6d3821086a7e69fdf5deac0ff498b8f03bbf8dc4
MamianS12 <38766984+MamianS12@users.noreply.github.com>
2018-11-14 14:45:57 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+* index.php
+*
+* standalone receiver
+* freestanding code to load the FrontController
+* requires the "config.inc.php"
+* Implment de Listener Pathern
+*/
+error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+require_once "config/config.inc.php";
+require_once "LitlePHP.class.php";
+require_once "FrontController.class.php";
+
+
+FrontController::execute();
+
+
+?>
+
+
