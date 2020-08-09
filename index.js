4a21ee35e84477fde34ba27b68b02552b52b6bb9
evolunnner <69194298+evolunnner@users.noreply.github.com>
2020-08-09 18:13:44 -0500

    0

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const matematicas = require("./matematicas.js");
 const os = require("os");
 const fs = require("fs");
+const http = require("http");
 
+const handleServer = function (req, res) {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.write("<h1>hola mundo</h1>");
+    res.end();
+}
+
+const server = http.createServer(handleServer);
+
+
+server.listen(3000, function () {
+ console.log("servidor en el puerto 3000");
+
+
+});
+
+
+
+
+
+
+
+/*crear un archivo dentro de la carpeta */
 fs.writeFile("./algo.txt", "linea 0", function (err) {
     if (err) {
         console.log(err);
@@ -9,6 +32,7 @@ fs.writeFile("./algo.txt", "linea 0", function (err) {
     console.log("archivo creado");
 });
 
+/*leer algo dentro de la carpeta */
 fs.readFile("./algo.txt", function (err, data) {
     if (err) {
         console.log("err");
@@ -22,8 +46,6 @@ console.log(matematicas.add0(2, 2));
 console.log(matematicas.add1(2, 2));
 console.log(matematicas.add2(2, 2));
 console.log(matematicas.add3(2, 2));
-
-
 console.log(os.cpus());/*sirve para saber las especificaiones de procesador y cuantos nucleos existen*/
 console.log(os.networkInterfaces());/* */
 console.log(os.arch());/* */
