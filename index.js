f69e324ace102a9fa30415fdf944ecfae5a42522
evolunnner <69194298+evolunnner@users.noreply.github.com>
2020-08-09 15:59:01 -0500

    0

---
+++ index.js
@@ -0,0 +1,6 @@
+const matematicas = require("./matematicas.js");
+
+console.log(matematicas.add0(2,2));
+console.log(matematicas.add1(2,2));
+console.log(matematicas.add2(2,2));
+console.log(matematicas.add3(2,2));
---
+++ matematicas.js
@@ -0,0 +1,25 @@
+function add0(m1, m2) {
+    return m1 + m2;
+}
+
+function add1(m1, m2) {
+    return m1 - m2;
+
+}
+
+function add2(m1, m2) {
+    return m1 * m2;
+}
+
+function add3(m1, m2){
+
+return m1/m2;
+
+}
+
+
+
+exports.add0 = add0;
+exports.add1 = add1;
+exports.add2 = add2;
+exports.add3 = add3;
